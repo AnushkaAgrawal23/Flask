ae8bcaf8bc31eefed51c84f62a3efe92e9f82901
AnushkaAgrawal23 <anushkaagrawal280@gmail.com>
2020-01-23 01:13:11 +0530

    Flask
    Flask Basics and Templates

---
+++ flasktt/app.py
@@ -0,0 +1,30 @@
+from flask import Flask,render_template,redirect,request
+
+#__name__ == __main__
+app = Flask(__name__)
+
+friends = ["Anushka","Saurav","Anish"]
+num = 5
+@app.route('/')
+def hello():
+	return render_template("index.html", my_friends = friends,number = num)
+
+@app.route('/about')
+def about():
+	return "<h1>About Page</h1>"
+
+@app.route('/home')
+def home():
+	return redirect('/')
+
+@app.route('/submit',methods = ['POST'])
+def submit_data():
+	if request.method == 'POST':
+		name = request.form['username']
+
+	return "<h1> Hello {}".format(name)
+
+
+if __name__ == '__main__':
+	app.debug = True
+	app.run()
